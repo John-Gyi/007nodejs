d9ffc7dcf42339e19b5235a040cddba1b7c590c5
John Gyi <johnzinkoko1151999@gmail.com>
2020-02-02 11:17:58 +0630

    add api

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,4 +1,95 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt=require('bcryptjs');
+var User=require('../../model/User');
+var Post=require('../../model/Post');
+var checkAuth=require('../middleware/check_auth');
+
+router.get('/list',checkAuth,function(req,res){
+  Post.find(function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else {
+      res.status(200).json({
+        posts:rtn
+      })
+    }
+  })
+})
+
+router.post('/add',checkAuth,function(req,res){
+  var post=new Post();
+  post.title=req.body.title,
+  post.content=req.body.content,
+  post.author=req.body.author
+
+  post.save(function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else {
+      res.status(200).jaon({
+        message:"Post Created",
+        posts:rtn
+      })
+    }
+  })
+})
+router.get('/:id',function(req,res){
+  Post.findById(req.params.id,function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else {
+      res.status(201).json({
+        posts:rtn
+      })
+    }
+
+  })
+})
+
+router.patch('/:id',function(req,res){
+  var update={};
+  for(var p of req.body){
+    update[p.proName]=p.proValue
+  }
+  console.log(update);
+
+  Post.findByIdAndUpdate(req.params.id,{$set:update},function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else{
+      res.status(200).json({
+        message:"Post  Updated"
+      })
+    }
+  })
+})
+
+router.delete('/:id',function(req,res){
+  Post.findByIdAndDelete(req.params.id,function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else{
+      res.status(200).json({
+        message:"Yours Post Deleted"
+      })
+    }
+  })
+})
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,9 +3,10 @@ var router = express.Router();
 var bcrypt=require('bcryptjs');
 var User=require('../../model/User');
 var Post=require('../../model/Post');
+var checkAuth=require('../middleware/check_auth');
 
 
-router.get('/list',function(req,res){
+router.get('/list',checkAuth,function(req,res){
   console.log('call');
   User.find(function(err,rtn){
     if(err){
@@ -16,6 +17,77 @@ router.get('/list',function(req,res){
     }else {
       res.status(200).json({
         users:rtn
+      })
+    }
+  })
+})
+
+router.post('/add',checkAuth,function(req,res){
+  var user=new User();
+  user.name=req.body.name,
+  user.email=req.body.email,
+  user.password=req.body.password
+
+  user.save(function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else {
+      res.status(201).json({
+        message:"User Account Created",
+        users:rtn
+      })
+    }
+  })
+})
+router.get('/:id',function(req,res){
+  User.findById(req.params.id,function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else {
+      res.status(201).json({
+        users:rtn
+      })
+    }
+
+  })
+})
+
+router.patch('/:id',function(req,res){
+  var update={};
+  for(var p of req.body){
+    update[p.proName]=p.proValue
+  }
+  console.log(update);
+
+  User.findByIdAndUpdate(req.params.id,{$set:update},function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else{
+      res.status(200).json({
+        message:"User Account Updated"
+      })
+    }
+  })
+})
+router.delete('/:id',function(req,res){
+  User.findByIdAndDelete(req.params.id,function(err,rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal Server Error",
+        error:err
+      })
+    }else{
+      res.status(200).json({
+        message:"User Account Deleted"
       })
     }
   })
